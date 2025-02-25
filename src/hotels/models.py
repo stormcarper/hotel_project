daf6a1e6c946e7bf7acf889431c566c1f880b452
stormcarper <storm@maykinmedia.nl>
2025-02-25 16:57:11 +0100

    room and geo added to models

--- src/hotels/models.py
+++ src/hotels/models.py
@@ -10,19 +10,28 @@ class Hotels(models.Model):
     directions = models.CharField(max_length=200, default='default_directions')
     phone = models.CharField(max_length=15, blank=True, null=True)
     tollfree = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
     fax = models.CharField(max_length=30, blank=True, null=True)
     url = models.URLField(blank=True, null=True)
     hours = models.CharField(max_length=5, blank=True, null=True)
     checkin = models.CharField(max_length=5, blank=True, null=True)
     checkout = models.CharField(max_length=5, blank=True, null=True)
-    image = models.CharField(blank=True, null=True)
-    price = models.CharField(max_length=10, blank=True, null=True)
+    image = models.CharField(max_length=255, blank=True, null=True)
+    price = models.CharField(max_length=25, blank=True, null=True)
     content = models.TextField(blank=True, null=True)
     activity = models.CharField(max_length=100, default='default_activity')
     type = models.CharField(max_length=20, default='default')
     availability = models.BooleanField(default=False)
 
+class Geo(models.Model):
+    geo_id = models.AutoField(primary_key=True)
+    hotel = models.OneToOneField(Hotels, on_delete=models.CASCADE, related_name='geo', null=True)
+    lat = models.CharField(max_length=20, default="default_lat")
+    lon = models.CharField(max_length=20, default="default_lon")
 
-
-
-    
+class Rooms(models.Model):
+    room_id = models.AutoField(primary_key=True)
+    hotel = models.ForeignKey(Hotels, on_delete=models.CASCADE, related_name='rooms', null=True)
+    room_type = models.CharField(max_length=50, default="default_type")
+    price = models.CharField(max_length=20, default="default_price")
+    availability = models.BooleanField(default=False)
